b315d99634cb318513a249b3d2b162bd11041cbc
Pablo Marino <pmarino2013@gmail.com>
2021-06-07 11:40:22 -0300

    Creando carpeta lista_dom y comentando el codigo

--- fundamentos/ejemplos/lista_dom/app.js
+++ fundamentos/ejemplos/lista_dom/app.js
@@ -7,20 +7,22 @@ let alumnos = [
   "Alberto Perez",
 ];
 
-let tarjeta = document.querySelector(".card-body");
-let tarjetaOrdena = document.querySelector("#card-ordenar");
+let tarjeta = document.querySelector(".card-body"); //obtengo la clase de la card-body
+let tarjetaOrdena = document.querySelector("#card-ordenar"); //obtener el id de card-ordenar
 
 function listarAlumnos(array, contenedor) {
   array.forEach(function (alumno) {
-    let contenido = document.createElement("p");
-    contenido.innerText = alumno;
+    let contenido = document.createElement("p"); //Creamos un elemento p
+    contenido.innerText = alumno; //Agregamos contenido al p. Nombre da cada alumno
 
-    return contenedor.appendChild(contenido);
+    return contenedor.appendChild(contenido); //Agregamos el contenido al contenedor
   });
 }
 
+//cargar dinámicamente la primera lista
 listarAlumnos(alumnos, tarjeta);
 
+//Obtener nombre y apellido. Listar con el apellido primero
 function intercalarNombre(lista) {
   let newList = lista.map(function (item) {
     let espacio = item.indexOf(" ");
@@ -33,6 +35,7 @@ function intercalarNombre(lista) {
   return newList;
 }
 
+//Ordenar lista de apellido nombre de forma alfabética (a-z)
 function ordenarAlumnos() {
   limpiarLista();
   let alumnosOrdenados = intercalarNombre(alumnos).sort();
@@ -48,5 +51,5 @@ function ordenarAlumnosZ() {
 }
 
 function limpiarLista() {
-  tarjetaOrdena.innerHTML = "";
+  tarjetaOrdena.innerHTML = ""; //limpiar el contenedor de la segunda lista
 }
